1533ac0b4f9f8672f02b710e621a7c41d7db1d8f
Will Browning <wdbrwnng@memphis.edu>
2024-02-06 13:21:10 -0600

    Refactored the code to instead use a function

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,8 +1,11 @@
+ 	
 
+def check_palindrome(text):
+   if text == text[::-1]:
+      print(f"{text} is a palindrome")
+   else:
+      print(f"{text} is not a palindrome")
 
 text = "did"
+check_palindrome(text)
 
-if text == text[::-1]:
-   print(f"{text} is a palindrome")
-else:
-   print(f"{text} is not a palindrome")
